455d75b51dc32b7d95a4fe8d84386bf005bf06e1
Gimmy <gamalgiali@gmail.com>
2021-04-08 14:04:23 -0400

    Django ORM with Docker, Postgres, W, and Wines

---
+++ server/catalog/views.py
@@ -0,0 +1,10 @@
+from rest_framework.generics import ListAPIView
+
+from .models import Wine
+from .serializers import WineSerializer
+from .filters import WineFilterSet
+
+class WinesView(ListAPIView):
+    queryset = Wine.objects.all()
+    serializer_class = WineSerializer
+    filterset_class = WineFilterSet
